eea90f95fd2b9ff4f24ca0f2e657fcff98bd99b7
Houssem TAYECH <houssem@eprofos.com>
2025-06-21 09:00:19 +0000

    feat: Refactor asset management and update template inheritance for public structure

---
+++ assets/public.js
@@ -0,0 +1,12 @@
+import './bootstrap.js';
+
+
+
+
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/public.css';
+
+
+// Import Bootstrap JavaScript
+import 'bootstrap';
--- importmap.php
+++ importmap.php
@@ -12,8 +12,8 @@
  * The "importmap:require" command can be used to add new entries to this file.
  */
 return [
-    'app' => [
-        'path' => './assets/app.js',
+    'public' => [
+        'path' => './assets/public.js',
         'entrypoint' => true,
     ],
     '@hotwired/stimulus' => [
